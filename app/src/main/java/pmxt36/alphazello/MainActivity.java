36b376f042c297d28a698c0ed231e61a656b6fbc
Shu Yang Chew <PMXT36@motorolasolutions.com>
2017-09-06 15:05:55 +0800

    init zello, and add feature to call, and select contacts on zello work app

--- app/src/main/java/pmxt36/alphazello/MainActivity.java
+++ app/src/main/java/pmxt36/alphazello/MainActivity.java
@@ -2,7 +2,14 @@ package pmxt36.alphazello;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.zello.sdk.Zello;
+import com.zello.sdk.Tab;
+import com.zello.sdk.Theme;
 
 /*
 NOTE: You need to have zello work app (not the one from appstore) installed using this link
@@ -16,13 +23,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Init Zello
+        Zello.getInstance().configure("net.loudtalks", this);
+
         // Find all available widgets
         Button pttButton = (Button) findViewById(R.id.ptt_button);
-        assert pttButton != null;
+
+
+        pttButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                // Initiate call using the account signed in on Zello Work app
+                // Todo: set contact programatically using result from image processing on first responder
+                // Sample : set target contact to shuyang's iphone
+
+                // Zello.getInstance().setSelectedUserOrGateway("yang");
+
+                int action = event.getAction();
+                if (action == MotionEvent. ACTION_DOWN ) {
+                    Zello.getInstance().beginMessage();
+                } else if (action == MotionEvent. ACTION_UP || action == MotionEvent. ACTION_CANCEL ) {
+                    Zello.getInstance().endMessage();
+                }
+                return false;
+            }
+        });
 
         Button selectButton = (Button) findViewById(R.id.select_button);
-        assert selectButton !=null;
 
-        
+
+        selectButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                // Let user select contacts
+                // TODO: Optional or let set programatically from code behind
+                Zello.getInstance().selectContact("Select a contact", new Tab[]{Tab.RECENTS,
+                        Tab.USERS, Tab.CHANNELS}, Tab.RECENTS, Theme.DARK);
+                return false;
+            }
+        });
     }
 }
